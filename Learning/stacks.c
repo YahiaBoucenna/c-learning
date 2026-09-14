96ce3b7b91a7a395919ba78edd18f59f30ac2e45
YahiaBoucenna <yahiahboc@gmail.com>
2026-09-14 18:19:56 +0100

    Add stack push/pop implementation

---
+++ Learning/stacks.c
@@ -0,0 +1,57 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+
+struct Node{
+    int data;
+    struct Node *next;
+};
+
+struct Node *push(struct Node *head,int data);
+struct Node *pop(struct Node *head,int *data);
+
+
+int main(void){
+
+
+
+    struct Node *head = malloc(sizeof(struct Node));
+    head->data = 1;
+    head->next = NULL;
+
+    head = push(head,4);
+    head = push(head,6);
+    head = push(head,8);
+
+    int val;
+    head = pop(head,&val);
+    head = pop(head,&val);
+
+    printf("head = %d",val);
+
+
+    
+
+
+
+
+}
+
+
+struct Node *push(struct Node *head,int value){
+    struct Node *newNode = malloc(sizeof(struct Node));
+    newNode->data = value;
+    newNode->next = head;
+    return newNode;
+}
+
+struct Node *pop(struct Node *head,int *value){
+    if(head != NULL){
+        *value = head->data;
+        struct Node *tmp = head->next;
+        free(head);
+        return tmp;
+    }
+    return 0;
+};
+ 
